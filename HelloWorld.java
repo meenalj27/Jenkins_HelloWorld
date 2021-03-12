466837c9345cad17437d44e9cdbb2c3cf98e4455
meenal singhal <meenalsinghal1994@gmail.com>
2021-03-13 02:14:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
 
-        System.out.println("Hello World Jenkins first test job to run .....New checking......");
+        System.out.println("Made changes......");
     }
 }
